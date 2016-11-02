4370963040fe1d567f3597a3bbc3b8cf38d3dd56
LudwigStengelin <ludwig@stengelin.org>
2016-11-02 12:53:55 +0100

    hallo

--- gurke/connect.php
+++ gurke/connect.php
@@ -5,15 +5,17 @@ $username = "u-jv029";
 $password = "IeBu2chie3";
 
 
-$conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "Connected successfully";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
 }
 catch(PDOException $e)
-    {
-        echo "Connection failed: " . $e->getMessage();
-    }
-?><?php
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
 /**
  * Created by PhpStorm.
  * User: Ludi
