07167e0f5eda694513639c5c6fb2753ec4a2f2a7
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-04 16:18:58 +0100

    test

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -31,7 +31,7 @@
             <li><a href="#">Einladen</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Einladen</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
             <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
         </ul>
     </div>
