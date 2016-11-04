03982740841e08e33ca45d13c10b0efaf812c63d
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-04 16:08:28 +0100

    test

---
+++ dateiuebersicht.php
@@ -0,0 +1,27 @@
+<!-- Start Include Dateien -->
+<?php include ("includes/bsfooter.php"); ?>
+<?php include ("includes/bseinbindung.php"); ?>
+<!-- Ende Include Dateien -->
+
+
+<!-- Start Dateiübersicht -->
+<?php ?>
+<!-- Ende Dateiübersicht -->
+
+
+<!-- --------------------------------------------------- PHP -> HTML ----------------------------------------------- -->
+
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dropbox - Dateiübersicht</title>
+
+</head>
+
+<body>
+
+</body>
+</html>
