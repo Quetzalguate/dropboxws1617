f867e0970e18a69d2e91e103d38b987a1f5e4f20
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-04 16:44:16 +0100

    test

---
+++ includes/bsfixednavbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Dropbox</a></li>
+                    <li><a href="#">Upload</a></li>
+                    <li><a href="#">Suche</a></li>
+                    <li><a href="#">Einladen</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
