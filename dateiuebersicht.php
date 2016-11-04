2e5a650688318e390baa848b9ce954b96b609604
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-04 16:38:53 +0100

    test

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -26,13 +26,13 @@
     <div class="container-fluid">
         <div class="row">
 
-                <ul class="nav nav-pills">
+                <ul class="nav-header">
                     <li class="active"><a href="#">Dropbox</a></li>
                     <li><a href="#">Upload</a></li>
                     <li><a href="#">Suche</a></li>
                     <li><a href="#">Einladen</a></li>
                 </ul>
-                <ul class="nav nav-pills navbar-right">
+                <ul class="nav-header navbar-right">
                     <li><a href="#"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
                     <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                 </ul>
