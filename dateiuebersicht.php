270f275c5aaa23586dc07a433913df50c0d23a3a
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-04 16:40:10 +0100

    test

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -26,13 +26,13 @@
     <div class="container-fluid">
         <div class="row">
 
-                <ul class="nav-header">
+                <ul class="nav navbar-nav">
                     <li class="active"><a href="#">Dropbox</a></li>
                     <li><a href="#">Upload</a></li>
                     <li><a href="#">Suche</a></li>
                     <li><a href="#">Einladen</a></li>
                 </ul>
-                <ul class="nav-header navbar-right">
+                <ul class="nav navbar-nav navbar-right">
                     <li><a href="#"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
                     <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                 </ul>
