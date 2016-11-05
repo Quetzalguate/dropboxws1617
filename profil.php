980210e6e2708a14039a6a4e45d837def3c021ea
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-06 00:52:27 +0100

    test

---
+++ profil.php
@@ -0,0 +1,34 @@
+<!-- Start Include Dateien -->
+<?php include ("includes/bsfixednavbar.php"); ?>
+<?php include ("includes/bsfooter.php"); ?>
+<?php include ("includes/bseinbindung.php"); ?>
+<!-- Ende Include Dateien -->
+
+
+<!-- Start Dateiübersicht -->
+<?php ?>
+<!-- Ende Dateiübersicht -->
+
+
+<!-- --------------------------------------------------- PHP -> HTML ----------------------------------------------- -->
+
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dropbox - Profil</title>
+
+</head>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <h2><u>Profilbild</u></h2>
+        </div>
+    </div>
+</div>
+<body>
+
+</body>
+</html>
