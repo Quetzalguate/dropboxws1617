6f7db56a000790f31b3bf892e9a30ade569d1381
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-06 01:12:34 +0100

    test

--- includes/bsfixednavbar.php
+++ includes/bsfixednavbar.php
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col-lg-12">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Dropbox</a></li>
+                    <li class="active"><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/dateiuebersicht.php">Dropbox</a></li>
                     <li><a href="#">Upload</a></li>
                     <li><a href="#">Suche</a></li>
                     <li><a href="#">Einladen</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
+                    <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/profil.php"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
                     <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                 </ul>
             </div>
