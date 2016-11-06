646b02d81b8b470df9d60c97ceb6dc089a4d3255
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-06 01:16:10 +0100

    test

---
+++ upload.php
@@ -0,0 +1,38 @@
+<!-- Start Include Dateien -->
+<?php include ("includes/bsfixednavbar.php"); ?>
+<?php include ("includes/bsfooter.php"); ?>
+<?php include ("includes/bseinbindung.php"); ?>
+<!-- Ende Include Dateien -->
+
+
+<!-- Start Dateiübersicht -->
+<?php ?>
+<!-- Ende Dateiübersicht -->
+
+
+<!-- --------------------------------------------------- PHP -> HTML ----------------------------------------------- -->
+
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dropbox - Profil</title>
+
+</head>
+</br></br>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h3><u>Datei hochladen</u></h3>
+            <label class="control-label">Datei auswählen</label>
+            <input id="input-1" type="file" class="file">
+            <button type="submit" class="btn btn-default">Datei hochladen</button>
+        </div>
+    </div>
+</div>
+<body>
+
+</body>
+</html>
