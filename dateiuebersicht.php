b3249595c0249ae08150f65fd3cd7043ebe746fa
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-08 14:23:50 +0100

    Juan

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -25,20 +25,25 @@
 <body>
 </br></br>
 <div class="container-fluid">
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th>Dateiname</th>
-                <th>Dateigröße</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Dateiname-Variable</td>
-                <td>Dateigröße-Variable</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col-lg-6">
+            <h3><u>Profilbild</u></h3>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Dateiname</th>
+                        <th>Dateigröße</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Dateiname-Variable</td>
+                        <td>Dateigröße-Variable</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 </body>
 </html>
