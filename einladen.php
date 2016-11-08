4359da739e23f17605c9a23f5f9b244bb21f3820
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-08 17:31:11 +0100

    Juan

---
+++ einladen.php
@@ -0,0 +1,39 @@
+<!-- Start Include Dateien -->
+<?php include ("includes/bsfixednavbar.php"); ?>
+<?php include ("includes/bsfooter.php"); ?>
+<?php include ("includes/bseinbindung.php"); ?>
+<!-- Ende Include Dateien -->
+
+
+<!-- Start Dateiübersicht -->
+<?php ?>
+<!-- Ende Dateiübersicht -->
+
+
+<!-- --------------------------------------------------- PHP -> HTML ----------------------------------------------- -->
+
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dropbox - Dateiübersicht</title>
+
+</head>
+
+<body>
+</br></br>
+<div class="container-fluid">
+    <h3><u>Freund einladen</u></h3>
+    <div class="form-group">
+        <label for="email">Email:</label>
+        <input type="text" class="form-control" id="email">
+    </div>
+    <div class="form-group">
+        <label for="comment">Nachricht</label>
+        <textarea class="form-control" rows="5" id="comment"></textarea>
+    </div>
+</div>
+</body>
+</html>
