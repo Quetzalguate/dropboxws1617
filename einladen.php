473a25f2d2c2c138f295b4d73e34436f2b264825
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-08 20:50:18 +0100

    Juan

--- einladen.php
+++ einladen.php
@@ -27,18 +27,21 @@
 <div class="container-fluid">
     <div class="col-lg-4">
         <h3><u>Freund einladen</u></h3>
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="email-deines-freundes@email.de">
-        </div>
-        <div class="form-group">
-            <label for="comment">Nachricht</label>
-            <textarea class="form-control" rows="5" id="comment">Hallo,
-Probier die neue Dropbox aus.
-Ich bin begeistert!
-Hier kannst du dich Registrieren: https://mars.iuk.hdm-stuttgart.de/~jv029/reg.php
-Wir sehen uns auf der anderen Seite ;-)</textarea>
-        </div>
+        <form>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" class="form-control" id="email" placeholder="email-deines-freundes@email.de">
+            </div>
+            <div class="form-group">
+                <label for="comment">Nachricht</label>
+                <textarea class="form-control" rows="5" id="comment">Hallo,
+    Probier die neue Dropbox aus.
+    Ich bin begeistert!
+    Hier kannst du dich Registrieren: https://mars.iuk.hdm-stuttgart.de/~jv029/reg.php
+    Wir sehen uns auf der anderen Seite ;-)</textarea>
+            </div>
+            <button type="submit" class="btn btn-default">Einladen</button>
+        </form>
     </div>
 </div>
 </body>
