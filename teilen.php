eadd28a0913a7cd8a211b1737ff3d399597a1325
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-08 22:00:31 +0100

    Juan

--- teilen.php
+++ teilen.php
@@ -27,7 +27,18 @@
 <div class="container-fluid">
     <div class="col-lg-4">
         <h3><u>Datei teilen</u></h3>
-
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Bereits geteilt mit:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>User-Variable</td>
+                    </tr>
+                </tbody>
+            </table>
     </div>
 </div>
 </body>
