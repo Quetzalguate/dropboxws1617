63e9717875bb108cb739a690d108cfe4575e79c9
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-08 21:10:31 +0100

    Juan

---
+++ suchen.php
@@ -0,0 +1,38 @@
+<!-- Start Include Dateien -->
+<?php include ("includes/bsfixednavbar.php"); ?>
+<?php include ("includes/bsfooter.php"); ?>
+<?php include ("includes/bseinbindung.php"); ?>
+<!-- Ende Include Dateien -->
+
+
+<!-- Start Dateiübersicht -->
+<?php ?>
+<!-- Ende Dateiübersicht -->
+
+
+<!-- --------------------------------------------------- PHP -> HTML ----------------------------------------------- -->
+
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dropbox - Suchen</title>
+
+</head>
+
+<body>
+</br></br>
+<div class="container">
+    <div class="input-group col-md-12">
+    <input type="text" class="form-control input-lg" placeholder="Buscar" />
+    <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="button">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+    </div>
+</div>
+</body>
+</html>
