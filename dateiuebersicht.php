2a2efdb23a4c70948a6cb0bbfdef34ed9f364374
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-08 15:47:35 +0100

    Juan

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -23,36 +23,34 @@
 </head>
 
 <body>
-</br></br>
-<div class="container-fluid">
-            <h3><u>Dateiübersicht</u></h3>
-
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th>Dateiname</th>
-                            <th>Dateigröße</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <div class="dropdown">
-                                    <a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-file"></span> Dateiname-Variable
-                                        <span class="caret"></span></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="#">Umbenennen</a></li>
-                                        <li><a href="#">Herunterladen</a></li>
-                                        <li><a href="#">Teilen</a></li>
-                                        <li><a href="#">Löschen</a></li>
-                                    </ul>
-                                </div>
-                            </td>
-                            <td>Dateigröße-Variable</td>
-                        </tr>
-                    </tbody>
-                </table>
-
-</div>
+    </br></br>
+    <div class="container-fluid">
+        <h3><u>Dateiübersicht</u></h3>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Dateiname</th>
+                    <th>Dateigröße</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-file"></span> Dateiname-Variable
+                                <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Umbenennen</a></li>
+                                <li><a href="#">Herunterladen</a></li>
+                                <li><a href="#">Teilen</a></li>
+                                <li><a href="#">Löschen</a></li>
+                            </ul>
+                        </div>
+                    </td>
+                    <td>Dateigröße-Variable</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
