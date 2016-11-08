2c28905a19a32aae54f9fe8dbd164013b4b25d2c
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-08 20:38:44 +0100

    Juan

--- einladen.php
+++ einladen.php
@@ -33,7 +33,7 @@
         </div>
         <div class="form-group">
             <label for="comment">Nachricht</label>
-            <textarea class="form-control" rows="5" id="comment"></textarea>
+            <textarea class="form-control" rows="5" id="comment" value="hallo"></textarea>
         </div>
     </div>
 </div>
