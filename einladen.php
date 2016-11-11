bcd64ae7134e2e92dc062e5721e72e48dcbdd9cd
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-11 23:34:10 +0100

    Juan

--- einladen.php
+++ einladen.php
@@ -62,7 +62,7 @@ if(isset($_POST['submit'])){
             <div class=\"container-fluid\">
                 <div class=\"col-lg-4\">
                     <div class=\"alert alert-success\">
-                        <a href=\"#" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>
+                        <a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>
                         <strong>Du hast deinen Freund erfolgreich eingeladen!</strong>
                     </div>
                 </div>
