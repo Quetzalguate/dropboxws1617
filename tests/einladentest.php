09b27142d4da3a432173f05365ad3b0f3dd215fd
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-11 18:16:53 +0100

    Juan

--- tests/einladentest.php
+++ tests/einladentest.php
@@ -12,8 +12,6 @@
 // the message
 $msg = "First line of text\nSecond line of text";
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
 
 // send email
 mail("juancavel93@gmail.com","My subject",$msg);
