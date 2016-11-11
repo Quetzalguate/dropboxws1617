629b0f6e12df3457ecfb65248109af7fa3d767c0
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-11 17:51:05 +0100

    Juan

---
+++ tests/einladentest.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="einladentest.php" method="post">
+    email: <input type="text" name="name"><br>
+    nachricht: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
