71e6c66a059540f731d0e8fd4af51557536e63c5
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-11 23:00:09 +0100

    Juan

--- einladen.php
+++ einladen.php
@@ -48,8 +48,7 @@ Wir sehen uns auf der anderen Seite ;-)</textarea>
 <?php
 $msg = $_POST['nachricht'];
 $email = $_POST['email'];
-echo $email;
-echo "hallo";
+echo $msg;
 if(isset($_POST['submit'])){
 
     mail("$email","Einladung zur Dropbox","$msg");
