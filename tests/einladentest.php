02e3eff15b204d1e17cc5dc15c5d476f9a590d1d
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-11 18:20:47 +0100

    Juan

--- tests/einladentest.php
+++ tests/einladentest.php
@@ -2,19 +2,20 @@
 <body>
 
 <form action="einladentest.php" method="post">
-    email: <input type="text" name="name"><br>
-    nachricht: <input type="text" name="email"><br>
+    email: <input type="text" name="email"><br>
+    nachricht: <input type="text" name="nachricht"><br>
     <input type="submit">
 </form>
 
 
 <?php
-// the message
-$msg = "First line of text\nSecond line of text";
+$msg = $_POST['nachricht'];
+$email = $_POST['email'];
+if(isset($submit)) {
 
+    mail($email,"My subject",$msg);
+}
 
-// send email
-mail("juancavel93@gmail.com","My subject",$msg);
 ?>
 
 </body>
