b74bb693f0d3c650ee5144c699df8a9648aae54c
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-17 23:11:13 +0100

    Juan

--- reg.php
+++ reg.php
@@ -5,12 +5,8 @@
 <!-- Ende Include Dateien -->
 
 
-<!-- Start Registrierung -->
-<?php ?>
-<!-- Ende Registrierung -->
 
 
-<!-- --------------------------------------------------- PHP -> HTML ----------------------------------------------- -->
 
 
 <!DOCTYPE html>
@@ -74,3 +70,65 @@
 
 </body>
 </html>
+
+<!-- --------------------------------------------------- HTML -> PHP ----------------------------------------------- -->
+
+
+
+<!-- Start Registrierung -->
+<?php
+session_start();
+$servername = "localhost";
+$username = "jv029";
+$password = "IeBu2chie3";
+
+$pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
+
+if(isset($_GET['register'])) {
+    $error = false;
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
+    $passwort2 = $_POST['passwort2'];
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+        $error = true;
+    }
+    if(strlen($passwort) == 0) {
+        echo 'Bitte ein Passwort angeben<br>';
+        $error = true;
+    }
+    if($passwort != $passwort2) {
+        echo 'Die Passwörter müssen übereinstimmen<br>';
+        $error = true;
+    }
+
+    //Überprüfe die E-Mail (einzigartig?)
+    if(!$error) {
+        $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+        $result = $statement->execute(array('email' => $email));
+        $user = $statement->fetch();
+
+        if($user !== false) {
+            echo 'Diese E-Mail-Adresse ist bereits vergeben<br>';
+            $error = true;
+        }
+    }
+
+    //Nutzer wird registriert
+    if(!$error) {
+        $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
+
+        $statement = $pdo->prepare("INSERT INTO users (email, passwort) VALUES (:email, :passwort)");
+        $result = $statement->execute(array('email' => $email, 'passwort' => $passwort_hash));
+
+        if($result) {
+            echo 'Du wurdest erfolgreich registriert. <a href="login.php">Zum Login</a>';
+            $showFormular = false;
+        } else {
+            echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+        }
+    }
+}
+?>
+<!-- Ende Registrierung -->
