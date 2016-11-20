3eda6a69fc5ced6b52aa687474b19b71c23fdf5a
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-20 21:42:05 +0100

    Juan

--- tests/dateiübersicht.php
+++ tests/dateiübersicht.php
@@ -6,10 +6,13 @@ $password = "IeBu2chie3";
 $pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
 $statement = $pdo->prepare("SELECT dateiname FROM dbzuordnung WHERE userid =1");
 $statement->execute();
-$dateiname = $statement->fetch();
+
+while ($dateiname = $statement->fetch() ){
+
+    echo $dateiname[0];}
 
 //var_dump($dateiname);
-echo $dateiname[0];
+
 
 //http://www.php-einfach.de/mysql-tutorial/komplexere-datenabfrage/
 ?>
