c1911e43a8c9027cb8a97b3f600f2355ae03fba7
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-20 17:23:26 +0100

    Juan

--- tests/dateiübersicht.php
+++ tests/dateiübersicht.php
@@ -5,7 +5,7 @@ $password = "IeBu2chie3";
 
 $pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
 $statement = $pdo->prepare("SELECT * FROM dbdateien WHERE dateiname = :email");
-$result = $statement->execute(array('email' => $email));
+$statement->execute();
 $dateiname = $statement->fetch();
 
 echo $dateiname;
