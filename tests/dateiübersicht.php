8228ada9896fadb3fce12b9bbaa2c421e97a36c4
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-20 19:05:30 +0100

    Juan

--- tests/dateiübersicht.php
+++ tests/dateiübersicht.php
@@ -4,7 +4,7 @@ $username = "jv029";
 $password = "IeBu2chie3";
 
 $pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
-$statement = $pdo->prepare("SELECT dateiname FROM dbdateien");
+$statement = $pdo->prepare("SELECT dateiname FROM dbzuordnung WHERE userid =1");
 $statement->execute();
 $dateiname = $statement->fetch();
 
