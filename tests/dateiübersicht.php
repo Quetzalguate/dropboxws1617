b40e6bf2b33c32d39f5c964d09e0b85cd28ecfcd
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-20 22:23:35 +0100

    Juan

--- tests/dateiübersicht.php
+++ tests/dateiübersicht.php
@@ -46,12 +46,12 @@ $statement->execute();
 
 <?php
             while ($dateiname = $statement->fetch() ){
-
+            $hallo = $dateiname[0];
             echo '
 
             <td>
                 <div class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-file"></span> $dateiname
+                    <a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-file"></span> $hallo
                         <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/umbenennen.php">Umbenennen</a></li>
