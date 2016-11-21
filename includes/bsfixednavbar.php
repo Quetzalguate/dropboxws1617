11cd20c51824ea7d3a4a1d477cd431fb7656f2fc
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-21 19:00:23 +0100

    Juan

--- includes/bsfixednavbar.php
+++ includes/bsfixednavbar.php
@@ -2,12 +2,6 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/dateiuebersicht.php">Dropbox</a></li>
                     <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/upload.php">Upload</a></li>
@@ -18,7 +12,6 @@
                     <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/profil.php"><span class="glyphicon glyphicon-user"></span> Profil</a></li>
                     <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                 </ul>
-                    </div>
             </div>
         </div>
     </div>
