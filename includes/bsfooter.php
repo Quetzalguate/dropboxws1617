b5c0bb92e2f96507eafeccdc1966a2da27da1277
Quetzalguate <jv029@hdm-stuttgart.de>
2016-11-21 18:52:14 +0100

    Juan

--- includes/bsfooter.php
+++ includes/bsfooter.php
@@ -1,8 +1,8 @@
 <!-- Anfang Footer -->
-<div class="container-fluid">
-    <div class="navbar navbar-fixed-bottom">
+<nav class="navbar navbar-fixed-bottom">
+    <div class="container-fluid">
         <p class="copyright small text-center">&copy; 2016/2017 Juan Carlos Velarde, Ludwig</p>
         <p class="small text-center"> Icons from <a href="https://icons8.com/">Icons8</a>.</p>
     </div>
-</div>
+</nav>
 <!-- Ende Footer -->
