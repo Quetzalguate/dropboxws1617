41695fd0c1751e2d82ac28ce4b641cdeafb46e68
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-14 00:37:29 +0100

    Juan

--- tests/jointest.php
+++ tests/jointest.php
@@ -9,8 +9,7 @@ $pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
 //$statement = $pdo->prepare("SELECT dateiname FROM dbdateien WHERE userid =1");
 $statement = $pdo->prepare("SELECT dbdateien.dateiname
                             FROM dbdateien JOIN dbteilen 
-                            ON dbdateien.userid = dbteilen.userid
-                            WHERE dbteilen.userid=1");
+                            ON dbdateien.userid = 1");
 $statement->execute();
 while ($result = $statement->fetch()){
     //var_dump($result);
