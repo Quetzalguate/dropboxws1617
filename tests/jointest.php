8a48565112930debeeb16e54ff96973ea739b25c
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-13 22:52:18 +0100

    Juan

--- tests/jointest.php
+++ tests/jointest.php
@@ -1,4 +1,8 @@
-<?php include ("home/includes/connection.php");
+<?php
+
+$servername = "localhost";
+$username = "jv029";
+$password = "IeBu2chie3";
 
 
 $pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
