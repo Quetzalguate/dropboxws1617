d420a2a59bbf0fcc89b603e574afe3cf8dfac780
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-14 00:16:07 +0100

    Juan

--- tests/jointest.php
+++ tests/jointest.php
@@ -8,9 +8,9 @@ $password = "IeBu2chie3";
 $pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
 //$statement = $pdo->prepare("SELECT dateiname FROM dbdateien WHERE userid =1");
 $statement = $pdo->prepare("SELECT dbdateien.dateiname
-                            FROM dbdateien INNER JOIN dbteilen 
+                            FROM dbdateien JOIN dbteilen 
                             ON dbdateien.userid = dbteilen.userid
-                            AND dbdateien.dateiid=dbteilen.dateiid");
+                            WHERE dbdateien.userid=1");
 $statement->execute();
 while ($result = $statement->fetch()){
     //var_dump($result);
