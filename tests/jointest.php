2a7af1aded6913ae2c650954333e361676531335
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-14 22:16:34 +0100

    Juan

--- tests/jointest.php
+++ tests/jointest.php
@@ -8,7 +8,7 @@ $password = "IeBu2chie3";
 $pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
 //$statement = $pdo->prepare("SELECT dateiname FROM dbdateien WHERE userid =1");
 $statement = $pdo->prepare("SELECT dbdateien.dateiname
-                            FROM dbdateien FULL OUTER JOIN dbteilen 
+                            FROM dbdateien LEFT OUTER JOIN dbteilen 
                             ON dbdateien.dateiid = dbteilen.dateiid
                             WHERE dbteilen.userid=1");
 $statement->execute();
