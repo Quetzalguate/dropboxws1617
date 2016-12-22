6547f2db32ce19686b660f373869050155ec9414
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-22 19:05:54 +0100

    Juan

---
+++ tests/test.php
@@ -0,0 +1,15 @@
+<!-- Start Bootstrap Einbindung -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- Ende Bootstrap Einbindung -->
+<?php
+$servername = "localhost";
+$username = "jv029";
+$password = "IeBu2chie3";
+
+$pdo = new PDO("mysql:host=$servername;dbname=u-jv029", $username, $password);
+$statement3 = $pdo->prepare ("INSERT INTO dbzuweisung (userid,dateiid) VALUES (3, 3)");
+$statement3->execute();
+?>
