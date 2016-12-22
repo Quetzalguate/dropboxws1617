6cb8704aa2795bff5b8a2833fe982b00eae3060c
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-22 18:14:57 +0100

    Juan

--- teilen.php
+++ teilen.php
@@ -52,7 +52,7 @@ $dateiname= $statement->fetch();
             </table>
         <form>
             <div class="form-group">
-                <label for="username">"<?php echo $dateiname[0]?>" mit Nutzer teilen:</label>
+                <label for="username">"<?php echo $dateiname[0];?>" mit Nutzer teilen:</label>
                 <input type="text" class="form-control" id="username" placeholder="Email deines Freundes eingeben">
             </div>
             <button type="submit" class="btn btn-default">Teilen</button>
