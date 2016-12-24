bc9df196f97c69048cb6b4c51075edfadb68e904
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-24 12:45:57 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -19,6 +19,6 @@ $useremail = $statement->fetch();
 echo $useremail[0];*/
 
 
-echo readfile("hashwertgollum1.jpg",' /home/jv029/public_html/upload/');
+echo readfile("hashwertgollum1.jpg",'/home/jv029/public_html/upload/');
 
 ?>
