2e8714c22d1890ea5d72bdeee1537f1564fc155e
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-26 01:18:34 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -12,7 +12,7 @@ $dir = "upload/";
 // Quick check to verify that the file exists
 if( !file_exists($file) ) die("File not found");
 // Force the download
-header("Content-Disposition: attachment; filename="" . basename($file) . """);
+header("Content-Disposition: attachment; filename=$file . basename($file) . """);
 header("Content-Length: " . filesize($file));
 header("Content-Type: application/octet-stream;");
 readfile($file,$dir);
