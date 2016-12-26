7862cafd97d4e38380d39d05fc3798a25546d6e4
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-26 12:35:16 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -13,11 +13,11 @@ if (file_exists($dir)) {
     echo "file exists";
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+    header('Content-Disposition: attachment; filename="'.basename($dir).'"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
-    header('Content-Length: ' . filesize($filename));
+    header('Content-Length: ' . filesize($dir));
     readfile($dir);
     exit;
 }
