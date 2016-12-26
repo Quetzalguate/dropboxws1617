d4d1f967deba05e2a6dcf8405df7e901d377b5ed
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-26 13:45:25 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -8,6 +8,7 @@
 
 $filename = 'gollum.jpg';
 $dir = "upload/".$filename;
+$dateiname = __DIR__ . '/gollum.jpg'
 //sftp://jv029@mars.iuk.hdm-stuttgart.de/home/jv029/public_html/tests/upload/gollum.jpg
 
 if (file_exists($dir)) {
@@ -21,7 +22,7 @@ if (file_exists($dir)) {
     header('Content-Length: ' . filesize($filename));
     ob_clean();
     flush();
-    readfile($filename,$dir);
+    readfile($dateiname);
     exit;
 }
 else
