30f3ad90025fe8ea98e14f5b9852baebb8759c2d
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-26 01:20:29 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -8,7 +8,7 @@
 
 
 $file = "gandalf.jpg";
-$dir = "upload/";
+$dir = "upload/".$filename;
 // Quick check to verify that the file exists
 if( !file_exists($file) ) die("File not found");
 // Force the download
