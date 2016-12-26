eaf7fb9ff88612ee6e2e3c4bc53b8013b6bc7f92
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-26 13:07:30 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -7,7 +7,7 @@
 <?php
 
 $filename = 'gollum.jpg';
-$dir = "sftp://jv029@mars.iuk.hdm-stuttgart.de/home/jv029/public_html/tests/upload/".$filename;
+$dir = "mars.iuk.hdm-stuttgart.de/home/jv029/public_html/tests/upload/".$filename;
 //sftp://jv029@mars.iuk.hdm-stuttgart.de/home/jv029/public_html/tests/upload/gollum.jpg
 
 if (file_exists($dir)) {
