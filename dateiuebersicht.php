bff59c9b30d4d877d67ebcaf9bd01f29d5ea27ec
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-27 23:37:00 +0100

    Juan

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -36,7 +36,8 @@
 
     <!-- Start Ergebnis der Datenbankabfrage per while Schleife in Tabelle ausgeben -->
     <div class="container-fluid">
-        <div class="col-md-3">
+        <div class="col-md-3"><div>
+        <div class="col-md-6">
         <h3><u>Dateiübersicht</u></h3>
         <table class="table table-hover">
             <thead>
