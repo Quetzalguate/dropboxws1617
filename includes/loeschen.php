68ed59e52afcbf9404bd6b5dcc325e6dcba831ad
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-27 01:16:36 +0100

    Juan

--- includes/loeschen.php
+++ includes/loeschen.php
@@ -26,18 +26,23 @@
     $dateiidzw = $dateiid[0];
 
     //Auslesen ob Besitzer gleich 0 oder 1
-    $stmt = $pdo->prepare("SELECT besitzer FROM dbzuweisung WHERE dateiid=:dateiidzw AND userid=:userid"); // Hier muss als Bedingung noch die userid im hashwert einbezogen werden, da ja der datainame nicht eindeutig ist
-    $stmt->bindParam(':dateiidzw', $dateiidzw, PDO::PARAM_STR);
-    $stmt->bindParam(':userid', $userid, PDO::PARAM_STR);
-    $stmt->execute();
-    $besitzer= $stmt->fetch();
+    $stmt2 = $pdo->prepare("SELECT besitzer FROM dbzuweisung WHERE dateiid=:dateiidzw AND userid=:userid"); // Hier muss als Bedingung noch die userid im hashwert einbezogen werden, da ja der datainame nicht eindeutig ist
+    $stmt2->bindParam(':dateiidzw', $dateiidzw, PDO::PARAM_STR);
+    $stmt2->bindParam(':userid', $userid, PDO::PARAM_STR);
+    $stmt2->execute();
+    $besitzer= $stmt2->fetch();
     echo $besitzer[0];
     $besitzerzw = $besitzer[0];
 
     //Wenn Besitzer = 1 dürfen die DB-Einträge und die Datei gelöscht werden
     if ($besitzerzw=1){
-        echo "besitzer gleich 1!";
+        echo "berechtigt datei zu löschen!";
         //DB-Eintrag aus dbdateien löschen --> Zeile löschen wo dateiname = xy
+        $stmt3 = $pdo->prepare("DELETE FROM dbdateien WHERE dateiname=:dateiname AND userid=:userid"); // Hier muss als Bedingung noch die userid im hashwert einbezogen werden, da ja der datainame nicht eindeutig ist
+        $stmt3->bindParam(':dateiname', $dateiname, PDO::PARAM_STR);
+        $stmt3->bindParam(':userid', $userid, PDO::PARAM_STR);
+        $stmt3->execute();
+
         //DB-Eintrag aus dbzuweisung löschen --> Zeile löschen wo dateiid = xy
 
         /*$datei = "../upload/gollum.jpg";
