5ac07477eed160ed8f3e279067f914020e2dd7ee
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-27 23:42:28 +0100

    Juan

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Dropbox - Dateiübersicht</title>
+    <title>MinimalBox - Dateiübersicht</title>
 
     <!-- Start Include Dateien -->
     <?php include ("includes/bsfixednavbar.php"); ?>
@@ -36,36 +36,38 @@
 
     <!-- Start Ergebnis der Datenbankabfrage per while Schleife in Tabelle ausgeben -->
     <div class="container-fluid">
-        <div class="col-md-6">
-        <h3><u>Dateiübersicht</u></h3>
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>Dateiname</th>
-                    <th>Dateigröße</th>
-                </tr>
-            </thead>
-            <tbody>
-            <?php while ($dateiname = $statement->fetch()) {?>
-                <tr>
-                    <td>
-                        <div class="dropdown">
-                            <a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-file"></span> <?php echo $dateiname[0]; ?>
-                                <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/umbenennen.php?var=<?php echo $dateiname[0]; ?>">Umbenennen</a></li>
-                                <li><a href="#">Herunterladen</a></li>
-                                <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/teilen.php?var=<?php echo $dateiname[0]; ?>">Teilen</a></li>
-                                <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/includes/loeschen.php?var=<?php echo $dateiname[0]; ?>">Löschen</a></li>
-                            </ul>
-                        </div>
-                    </td>
-                    <td>Dateigröße-Variable</td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-        </div>
+        <div class="row">
+            <div class="col-md-6">
+            <h3><u>Dateiübersicht</u></h3>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Dateiname</th>
+                            <th>Dateigröße</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php while ($dateiname = $statement->fetch()) {?>
+                        <tr>
+                            <td>
+                                <div class="dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-file"></span> <?php echo $dateiname[0]; ?>
+                                        <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/umbenennen.php?var=<?php echo $dateiname[0]; ?>">Umbenennen</a></li>
+                                        <li><a href="#">Herunterladen</a></li>
+                                        <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/teilen.php?var=<?php echo $dateiname[0]; ?>">Teilen</a></li>
+                                        <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/includes/loeschen.php?var=<?php echo $dateiname[0]; ?>">Löschen</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                            <td>Dateigröße-Variable</td>
+                        </tr>
+                    <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+            <div>
     </div>
     <!-- Ende Ergebnis der Datenbankabfrage per while Schleife in Tabelle ausgeben -->
 </body>
