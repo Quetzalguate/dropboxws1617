e54630885a2027f297025fce916cd39b63b03d1a
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-27 20:38:52 +0100

    Juan

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -12,6 +12,7 @@
     <?php include ("includes/connection.php"); ?>
     <!-- Ende Include Dateien -->
 
+
 </head>
 
 <body>
--- includes/bseinbindung.php
+++ includes/bseinbindung.php
@@ -1,6 +1,7 @@
 <!-- Start Bootstrap Einbindung -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/custom.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- Ende Bootstrap Einbindung -->
