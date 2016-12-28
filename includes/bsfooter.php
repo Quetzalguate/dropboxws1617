b88f20ffb1398160d19ac62cf2b6f8ec4af14e6c
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-28 15:49:25 +0100

    Juan

--- includes/bsfooter.php
+++ includes/bsfooter.php
@@ -1,7 +1,10 @@
 <!-- Anfang Footer -->
 <nav class="navbar navbar-fixed-bottom">
     <div class="container-fluid">
-        <span class="label label-default" class="text-center">Freier Speicher: x mb</span>
+        <div class="text-right">
+            <span class="label label-default">Freier Speicher: x mb</span>
+            <label >Freier Speicher: x mb</label>
+        </div>
         <p class="copyright small text-center">&copy; 2016/2017 Juan Carlos Velarde, Ludwig Stengelin</p>
         <p class="small text-center"> Icons from <a href="https://icons8.com/">Icons8</a>.</p>
     </div>
