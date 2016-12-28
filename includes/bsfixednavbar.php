ae8894b4d9596592a46d2ebf160f68c807aac857
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-28 15:26:03 +0100

    Juan

--- includes/bsfixednavbar.php
+++ includes/bsfixednavbar.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/dateiuebersicht.php">Dropbox</a></li>
+                    <li class="active"><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/dateiuebersicht.php"><img class="icon icons8-Home" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAABg0lEQVRoQ+2Z3W0CMRCEhwpIKSkBSkgHSQcpJR1AB5QAJaSUpIJEI2GB0PnGu17rAK1feMHr+fbHwx0rPPhaPbh+JMDSFcwKPHsFdmfAj1GgI1uI4t/PwvcAhkCMAHgBcACwucn6CcAbgJ/IakQDUPwRwGtF5DeAbSREJABFs21q4gsTIdhO/OxeUQAUzcyzAi2LbcRKdENEALDX2fOt4gsgITgTnA336gXgLVOuSq8IthNvKdfqAYgQX0S7IbwA13e8K3MTm1xe4QEYIb7wmCEsADWDiqpAiWMyvFYAZVDREM2G1wLQalAjIKThKQCrQUVDSMObA/Aa1AiIquHVACLv+CigSa+YArhH8VXDUzPAjX9RKXTGmdWYAM6sWrZlBawzoNoyNJ46zDPEKmYCXA+QylZWYOK6yRbKFhIupOYqWyhbKFvI8lMO839EqoHjUXwuXRvOVDEtQ/yr3rmqw6ibT2hfBggVsxWA4j/Ve1N1mCHxy3w1AZbJ++XUrEBWoDMD/4NPUDGmvbedAAAAAElFTkSuQmCC" width="15" height="15"> MinimalBox</a></li>
                     <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/upload.php">Upload</a></li>
                     <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/suchen.php">Suche</a></li>
                     <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/einladen.php">Einladen</a></li>
