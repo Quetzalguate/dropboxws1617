b3bfe34b202b827cd50167b3b8126d00dff02f25
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-28 17:07:57 +0100

    Juan

--- upload.php
+++ upload.php
@@ -24,7 +24,7 @@
 </br></br>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-12 align="center"" >
+        <div class="col-lg-12" align="center">
                 <h3>Lade eine Datei hoch</h3>
                 <form align="center">
                     <input id="input-1" type="file" class="file">
