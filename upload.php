6ee0f8f02379c4d58ecfdada6776aef7182432f6
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-28 17:11:07 +0100

    Juan

--- upload.php
+++ upload.php
@@ -1,40 +1,32 @@
-<!-- Start Include Dateien -->
-<?php include ("includes/bsfixednavbar.php"); ?>
-<?php include ("includes/bsfooter.php"); ?>
-<?php include ("includes/bseinbindung.php"); ?>
-<!-- Ende Include Dateien -->
-
-
-<!-- Start Dateiübersicht -->
-<?php ?>
-<!-- Ende Dateiübersicht -->
-
-
-<!-- --------------------------------------------------- PHP -> HTML ----------------------------------------------- -->
-
-
 <!DOCTYPE html>
 <html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>MinimalBox - Profil</title>
 
-<head>
-    <meta charset="UTF-8">
-    <title>MinimalBox - Profil</title>
+        <!-- Start Include Dateien -->
+        <?php include ("includes/bsfixednavbar.php"); ?>
+        <?php include ("includes/bsfooter.php"); ?>
+        <?php include ("includes/bseinbindung.php"); ?>
+        <!-- Ende Include Dateien -->
 
-</head>
-</br></br>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-lg-12" align="center">
-                <h3>Lade eine Datei hoch</h3>
-                <form>
-                    <input id="input-1" type="file" class="file" align="center">
-                    </br>
-                    <button type="submit" class="btn btn-default">Datei hochladen</button>
-                </form>
+    </head>
+    <body>
+        </br></br>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12" align="center">
+                    <h3>Lade eine Datei hoch</h3>
+                    <form>
+                        <input id="input-1" type="file" class="file" align="center">
+                        </br>
+                        <button type="submit" class="btn btn-default">Datei hochladen</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<body>
 
-</body>
+        <!-- --------------------------------------------------- HTML -> PHP ----------------------------------------------- -->
+
+    </body>
 </html>
