d48cc618e35b5013a25c4420347f544e6f9c776c
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-28 16:59:24 +0100

    Juan

--- upload.php
+++ upload.php
@@ -24,14 +24,16 @@
 </br></br>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-12" class="text-center">
+        <div class="col-lg-12" >
             <h3>Lade eine Datei hoch</h3>
-            <label class="control-label">Datei auswählen</label>
-            <form>
-                <input id="input-1" type="file" class="file">
-                </br>
-                <button type="submit" class="btn btn-default">Datei hochladen</button>
-            </form>
+            <div class="text-center">
+                <form>
+                    <input id="input-1" type="file" class="file">
+                    </br>
+                    <button type="submit" class="btn btn-default">Datei hochladen</button>
+                </form>
+            </div>
+
         </div>
     </div>
 </div>
