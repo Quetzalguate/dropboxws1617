df307e84ccd9186acac862398c85091420a60d0f
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-28 01:29:49 +0100

    Juan

--- includes/bsfixednavbar.php
+++ includes/bsfixednavbar.php
@@ -14,8 +14,11 @@
                 </ul>
             </div>
             <div class="row">
-                <div class="col-lg-11">
+                <div class="col-lg-1">
                     <label>Belegter Speicher: </label>
+                </div>
+                <div class="col-lg-10">
+
                     <div class="progress" class="text-left">
                         <div class="progress-bar" role="progressbar" aria-valuenow="70"
                              aria-valuemin="0" aria-valuemax="100" style="width:70%">
