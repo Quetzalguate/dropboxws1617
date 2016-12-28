92280cc6bc694b86ec1b2a9ca9cf1f417621b1cc
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-29 00:51:30 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -25,7 +25,7 @@
 </div>
 
 <div class="btn-group">
-    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+    <button type="button" class="btn btn-default dropdown-toggle btn-xs" data-toggle="dropdown">
         Licht <span class="caret"></span></button>
     <ul class="dropdown-menu" role="menu">
         <li><a href="#">An</a></li>
