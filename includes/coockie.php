19acc0bc7a21f60920e70c509ed95e9bfd593757
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-29 13:38:37 +0100

    Juan

---
+++ includes/coockie.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>MinimalBox - Coockie</title>
+
+    <!-- Start Include Dateien -->
+    <?php include ("/home/jv029/public_html/includes/connection.php"); ?>
+    <!-- Ende Include Dateien -->
+
+</head>
+
+<body>
+<?php
+
+session_start();
+if(!isset($_SESSION['email'])) {
+    die('Bitte zuerst <a href="login.php">einloggen</a>');
+}
+else {
+    $email = $_SESSION['email'];
+
+    $stmt = $pdo->prepare("SELECT userid FROM dbuser WHERE email=:email");
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+    $erg= $stmt->fetch();
+    $userid = $erg[0];
+}
+
+?>
+</body>
