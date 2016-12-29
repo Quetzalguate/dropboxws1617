03549f0f7a65f6cc71317ac72db0f1d6cbb4bbc1
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-29 01:52:07 +0100

    Juan

--- includes/bsfixednavbar.php
+++ includes/bsfixednavbar.php
@@ -20,7 +20,7 @@
                     <button type="button" class="btn btn-default dropdown-toggle btn-xs" data-toggle="dropdown">
                         Licht <span class="caret"></span></button>
                     <ul class="dropdown-menu" role="menu">
-                        <li onclick="document.body.style.backgroundColor = 'green';">an</li><!-- https://mars.iuk.hdm-stuttgart.de/~jv029/includes/background1.php?var=1-->
+                        <li ><a href="#" onclick="document.body.style.backgroundColor = 'green';"></a>an</li><!-- https://mars.iuk.hdm-stuttgart.de/~jv029/includes/background1.php?var=1-->
                         <li><button onclick="document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';">aus</button></li><!-- <li><a href="https://mars.iuk.hdm-stuttgart.de/~jv029/includes/background1.php?var=0">Aus</a></li>-->
                     </ul>
                 </div>
