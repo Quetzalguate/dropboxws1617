8e6bb201aae95c3c4dfa3173ceb817249275d4ad
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-30 18:10:55 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -54,7 +54,7 @@ foreach($result as $show){
 
 //2.3 Das letzte "+" des strings loeschen
 $summe = substr($ergforeach, 0, -1);
-
+echo "</br>".$summe;
 //2.4 Die Summe der Dateigroessen vom verfügbaren Speicher abziehen und freien Speicher ausgeben
 $freierspeicher = 20 - $summe;
 echo "Du hast noch: ".$freierspeicher ." mb frei!";
