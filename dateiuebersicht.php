2869417be914e842773e43219d13ba2d8835aa3c
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-30 23:47:09 +0100

    Juan

--- dateiuebersicht.php
+++ dateiuebersicht.php
@@ -8,7 +8,7 @@
     <!-- Start Include Dateien -->
     <?php include ("includes/coockie.php"); ?>
     <?php include ("includes/bsfixednavbar.php"); ?>
-    <?php include ("includes/bsfooter.php"); ?>
+    <?php// include ("includes/bsfooter.php"); ?>
     <?php include ("includes/bseinbindung.php"); ?>
     <?php include ("includes/connection.php"); ?>
     <?php echo $_COOKIE["background"]; ?>
