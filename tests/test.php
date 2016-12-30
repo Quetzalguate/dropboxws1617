4d9616ea1025540eda6fd5a1d22752708a8728cd
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-30 14:55:38 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -51,8 +51,9 @@ echo "</br>";
 //var_dump($erg);
 
 while($erg= $stmt3->fetch()){
-    echo $erg[0]."+";
+    $summe = $erg[0]."+";
 }
+echo $summe;
 /*
 foreach($erg AS $name) {
     echo $name.", ";
