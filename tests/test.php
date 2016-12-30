0d3500183b64132cdbded5da54c233f91b24ae52
Quetzalguate <jv029@hdm-stuttgart.de>
2016-12-30 15:04:17 +0100

    Juan

--- tests/test.php
+++ tests/test.php
@@ -46,8 +46,7 @@ $stmt3 = $pdo->prepare("SELECT dbdateien.dateigroesse
                             ON dbdateien.dateiid = dbzuweisung.dateiid
                             WHERE dbzuweisung.userid=$userid");
 $stmt3->execute();
-$erg= $stmt3->fetchAll();
-print_r($erg);
+$erg= $stmt3->fetch();
 
 echo "</br>";
 
