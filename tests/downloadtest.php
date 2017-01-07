4062f45c6ea4de5ac53c8422fac5ed88929e530c
Quetzalguate <jv029@hdm-stuttgart.de>
2017-01-08 00:34:10 +0100

    Juan

--- tests/downloadtest.php
+++ tests/downloadtest.php
@@ -1,22 +1,21 @@
 <?php
-$filename = 'hashwert1.jpg';
-$dir = "upload/".$filename;
-//sftp://jv029@mars.iuk.hdm-stuttgart.de/home/jv029/public_html/tests/upload/gollum.jpg
+$fileName = basename('hashwert1.jpg');
+$filePath = 'upload/'.$fileName;
+if(!empty($fileName) && file_exists($filePath)){
+    // Define headers
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=$fileName");
+    header("Content-Type: application/zip"); //Content-Type: image/jpeg
+    header("Content-Transfer-Encoding: binary");
 
-if (file_exists($dir)) {
-    echo "file exists";
-    header('Content-Description: File Transfer');
-    header('Content-Type: image/jpeg');
-    header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($filename));
-    ob_clean();
-    readfile($filename,$dir);
+    // Read the file
+    readfile($filePath);
     exit;
+}else{
+    echo 'The file does not exist.';
 }
-else
-    echo " file doesnt exist";
+
+
 
 ?>
