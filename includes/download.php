6aff6a030374ab84018d38e5e82467a4c41d8429
Quetzalguate <jv029@hdm-stuttgart.de>
2017-01-08 15:32:00 +0100

    Juan

--- includes/download.php
+++ includes/download.php
@@ -19,7 +19,7 @@ $stmt->bindParam(':dateiname', $dateiname, PDO::PARAM_STR);
 $stmt->execute();
 $erg= $stmt->fetch();
 $dateihash = $erg[0];
-echo $dateihash;
+echo "</br>".$dateihash;
 
 /*
 $basename = basename($dateihash);
