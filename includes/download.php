42ae01f3c24989ddbc277fbe361dffd61ca5d080
Quetzalguate <jv029@hdm-stuttgart.de>
2017-01-08 12:52:42 +0100

    Juan

--- includes/download.php
+++ includes/download.php
@@ -1,27 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>MinimalBox - Herunterladen</title>
+
+    <!-- Start Include Dateien -->
+    <?php include ("/home/jv029/public_html/includes/connection.php"); ?>
+    <!-- Ende Include Dateien -->
+</head>
+<body>
 <?php
-$name = $_GET['var'];
-$dateiname = basename("$name");
-$pfad = '/home/jv029/public_html/upload/'.$dateiname;
-if(!empty($dateiname) && file_exists($pfad)){
+$dateiname = $_GET['var'];
+
+//DB Abfrage um den zum Dateinamen zugehörigen hashwert auszulesen (Datei ist ja mit hashwert auf dem server gespeichert
+$stmt = $pdo->prepare("SELECT dateihash FROM dbdateien WHERE dateiname=:dateiname");
+$stmt->bindParam(':dateiname', $dateiname, PDO::PARAM_STR);
+$stmt->execute();
+$erg= $stmt->fetch();
+$dateihash = $erg[0];
+
+//eventuell rename
+
+$basename = basename("$dateihash");
+$pfad = '/home/jv029/public_html/upload/'.$basename;
+if(!empty($basename) && file_exists($pfad)){
     // Header definieren
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
-    header("Content-Disposition: attachment; filename=$dateiname");
+    header("Content-Disposition: attachment; filename=$basename");
     header("Content-Type: application/octet-stream"); //--> Hat funktioniert obwohl kein zip download!!! Content-Type:  application/zip     image/jpeg
     header("Content-Transfer-Encoding: binary");
 
     // Datei auslesen
     readfile($pfad);
-    echo "
-                    <meta http-equiv=\"refresh\" content=\"2; url=https://mars.iuk.hdm-stuttgart.de/~jv029/dateiuebersicht.php
-                ";
+    echo "<meta http-equiv=\"refresh\" content=\"2; url=https://mars.iuk.hdm-stuttgart.de/~jv029/dateiuebersicht.php";
     exit;
 }else{
     echo 'The file '.$name.' does not exist.';
-    if(file_exists($pfad)){
-        echo "</br>existiert!";
-    }
-    else{echo "</br>existiert nicht";}
 }
 
 ?>
