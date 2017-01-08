365d49c44d9dfd09741a0c1634aca07c98502847
Quetzalguate <jv029@hdm-stuttgart.de>
2017-01-08 15:19:06 +0100

    Juan

--- includes/download.php
+++ includes/download.php
@@ -12,30 +12,5 @@
 <?php
 $dateiname = $_GET['var'];
 
-//DB Abfrage um den zum Dateinamen zugehörigen hashwert auszulesen (Datei ist ja mit hashwert auf dem server gespeichert
-$stmt = $pdo->prepare("SELECT dateihash FROM dbdateien WHERE dateiname=:dateiname");
-$stmt->bindParam(':dateiname', $dateiname, PDO::PARAM_STR);
-$stmt->execute();
-$erg= $stmt->fetch();
-$dateihash = $erg[0];
-
-
-$basename = basename($dateihash);
-$pfad = '/home/jv029/public_html/upload/'.$basename;
-$mimetype = mime_content_type($pfad);
-if(!empty($basename) && file_exists($pfad)){
-    // Define headers
-    header("Cache-Control: public");
-    header("Content-Description: File Transfer");
-    header("Content-Disposition: attachment; filename=$basename");
-    header("Content-Type: $mimetype");
-    header("Content-Transfer-Encoding: binary");
-
-    // Read the file
-    readfile($pfad);
-    exit;
-}else{
-    echo 'The file does not exist.';
-}
-
+echo $dateiname;
 ?>
