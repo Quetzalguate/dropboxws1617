7f15e6b551f1f3fbc131d7e8496be009567357a4
Quetzalguate <jv029@hdm-stuttgart.de>
2017-01-08 14:18:04 +0100

    Juan

--- tests/downloadtest.php
+++ tests/downloadtest.php
@@ -5,7 +5,7 @@
     <title>MinimalBox - Herunterladen</title>
 
     <!-- Start Include Dateien -->
-    <?php include ("/home/jv029/public_html/includes/connection.php"); ?>
+    <?php //include ("/home/jv029/public_html/includes/connection.php"); ?>
     <!-- Ende Include Dateien -->
 </head>
 <body>
