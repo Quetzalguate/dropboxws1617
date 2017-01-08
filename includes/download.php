57a92ad50657d74a4c079a627bc81f37d078e962
Quetzalguate <jv029@hdm-stuttgart.de>
2017-01-08 15:49:59 +0100

    Juan

--- includes/download.php
+++ includes/download.php
@@ -10,16 +10,16 @@
 </head>
 <body>
 <?php
-$dateiname = $_GET['var'];
-echo $dateiname;
-//$dateiname = "schmeagol.jpg";//
+//$dateiname = $_GET['var'];
+//echo $dateiname;
+$dateiname = "schmeagol.jpg";
 
 $stmt = $pdo->prepare("SELECT dateihash FROM dbdateien WHERE dateiname=:dateiname");
 $stmt->bindParam(':dateiname', $dateiname, PDO::PARAM_STR);
 $stmt->execute();
 $erg= $stmt->fetch();
 $dateihash = $erg[0];
-echo "</br>".$dateihash;
+//echo "</br>".$dateihash;
 
 
 $basename = basename($dateihash);
@@ -27,7 +27,7 @@ $pfad = '/home/jv029/public_html/upload/'.$basename;
 $mimetype = mime_content_type($pfad);
 echo "</br>".$mimetype;
 if(!empty($basename) && file_exists($pfad)){
-    echo "</br>erfüllt if bedingung";
+    //echo "</br>if bedingung erfüllt";
     // Define headers
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
