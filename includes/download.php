48523354e2e6e7b64cec3043cab2205d53a12280
Quetzalguate <jv029@hdm-stuttgart.de>
2017-01-08 01:00:23 +0100

    Juan

---
+++ includes/download.php
@@ -0,0 +1,23 @@
+<?php
+$name = $_GET['var'];
+$dateiname = basename($name);
+$pfad = 'upload/'.$dateiname;
+if(!empty($dateiname) && file_exists($pfad)){
+    // Header definieren
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=$dateiname");
+    header("Content-Type: application/octet-stream"); //--> Hat funktioniert obwohl kein zip download!!! Content-Type:  application/zip     image/jpeg
+    header("Content-Transfer-Encoding: binary");
+
+    // Datei auslesen
+    readfile($pfad);
+    echo "
+                    <meta http-equiv=\"refresh\" content=\"2; url=https://mars.iuk.hdm-stuttgart.de/~jv029/dateiuebersicht.php
+                ";
+    exit;
+}else{
+    echo 'The file does not exist.';
+}
+
+?>
